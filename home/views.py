8e37b8fa658790e0dc2a06b4ccdd9e6915098710
davewatters <89341297+davewatters@users.noreply.github.com>
2022-04-26 18:16:45 +0100

    add homepage block content

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     '''
-    View to return the inex page
+    View to return the index page
     '''
     return render(request, 'home/index.html')
