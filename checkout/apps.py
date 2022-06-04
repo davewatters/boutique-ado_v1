a5e2fbe412ec1f360c3b49d2d839f9780e6151cd
davewatters <89341297+davewatters@users.noreply.github.com>
2022-06-04 16:41:13 +0100

    add checkout success logic

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     name = 'checkout'
 
-
     # Lets Django know that there's a new signals
     # model with some listeners in it
     def ready(self):
